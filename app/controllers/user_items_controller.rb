38ad38d04e4395108b7a62dd1ec111c8c29d24db
ikuko Schoeller <Ikuko@ikukos-mbp.local>
2017-09-30 23:14:46 -0700

    Update user_itmes_controller and user_items.index.html.erb git statu

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,9 +1,21 @@
 class UserItemsController < ApplicationController
 
-def create
-  # p "*" * 100 checked the route from add button and working fine.
-  # UserItem.create(user_id: current_user.id, item_id: Will pass from front end)
+  def index
+  	# I need to check with Edgar how to call cunrrent_user
+  	# @item_list = current_user.items
+  end
+
+	def create
+	  # p "*" * 100 checked the route from add button and working fine.
+	  # UserItem.create(user_id: current_user.id, item_id: Will pass from front end)
+
+	end
 
 end
 
-end
+
+# Paste to user_items.index.html.erb
+
+# <% @item_list.pluck(:name).each do |item_name| %>
+# 	<%= item_name %> <%= link_to 'delete this item', user_items_path(We are at index page so user_items do not have id on url. Where shold I sent to delete this list(use_items)??? ), method: :delete, data: { confirm: 'Are you sure?' } %>
+# <% end %>
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 	
-  resources :categories do
-  	resources :items, only: [:index]
-  end
-
+  resources :categories
+  	
   resources :user_items
- 
 
 	root 'categories#index'
 
