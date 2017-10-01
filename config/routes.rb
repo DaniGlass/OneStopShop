fdcfc3788cbb9031fdcd01d722947aed1ee522aa
ikuko Schoeller <Ikuko@ikukos-mbp.local>
2017-09-30 19:07:38 -0700

    Create categories controller and index and show page. From show page you can access to User Item create route when you click the add button

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+  resources :categories do
+  	resources :items, only: [:index]
+  end
+
+  resources :user_items
+  
+  # get 'list/index' => 'list#index'
+  # post 'list/new' => 'list#new'
+
+	root 'categories#index'
+
 end
