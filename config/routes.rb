41907385ff18db63c593e6c477043a2c8b8cdd78
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-09-30 21:53:33 -0700

    Show page appears now

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-	
+
   resources :categories do
   	resources :items, only: [:index]
   end
 
   resources :user_items
- 
+
+  resources :users, only: [:show]
+
 
 	root 'categories#index'
 
