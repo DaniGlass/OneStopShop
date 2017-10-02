d8dce4c8b94c1f5f81f88e49e4f53f52d81246a4
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-02 10:18:35 -0700

    Finds 3 stores where the user's whole list is available, sorted by price

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,8 +2,6 @@ class ResultsController < ApplicationController
   def index
     user = User.find(params[:id])
 
-    p "*" * 100
-    p @stores = user.find_stores
-    p ":)" * 100
+    render json: user.results.to_json
   end
 end
