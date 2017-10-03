9ce3989a016e7d5c3f2c73e0a1f071faa2be537d
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-02 22:09:57 -0700

    Make custom response for filtered results by lowest price

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,29 +4,6 @@ class User < ApplicationRecord
   has_many :user_items
   has_many :items, through: :user_items
 
-  def find_stores
-    # check all stores to see if they carry all the list
-    Store.all.select do |store|
-      store.has_all_of_users_list?(self.items)
-    end
-  end
-
-  def get_prices_from_stores
-    stores_where_list_is_available = self.find_stores
-
-    store_prices = {}
-
-    stores_where_list_is_available.each do |store|
-      store_prices[store.name] = StorePrice.where(item: self.items, store: store).pluck(:price).reduce(:+)
-    end
-
-    store_prices
-  end
-
-  def results
-    self.get_prices_from_stores.sort_by { |store_name, total_price_per_store| total_price_per_store }[0..2]
-  end
-
 
   def results
     results = []
