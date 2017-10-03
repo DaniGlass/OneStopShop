fcaae228cb45dba89361172f971aeaee30986d95
Jennifer Shopsin <jennifer.shopsin@gmail.com>
2017-10-03 11:40:27 -0700

    Add store details to pickstop screen

--- front_end/app/components/StoreResults/StoreResults.js
+++ front_end/app/components/StoreResults/StoreResults.js
@@ -3,18 +3,19 @@ import { View, Text, Image } from 'react-native';
 import axios from 'axios';
 
 import colors from '../../config/colors';
+import styles from './styles';
 
 class StoreResults extends Component {
   render() {
     return (
       <View style={styles.row}>
         <Image
-          source={{ uri: this.props.store.image_url }}
+          source={{ uri: this.props.store[1].closest_store.image_url }}
           style={styles.image}
         />
         <View style={styles.descriptionContainer}>
-          <Text style={styles.name}>{this.props.store.name}</Text>
-          <Text style={styles.description}>{this.props.store.description}</Text>
+          <Text style={styles.name}>{this.props.store[1].closest_store.name}</Text>
+          <Text style={styles.name}>{this.props.store[1].closest_store.name}</Text>
         </View>
       </View>
     );
