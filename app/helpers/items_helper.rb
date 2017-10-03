22ad89da8a0b6920dc82da7a2aeb000913e16d7e
Jennifer Shopsin <jennifer.shopsin@gmail.com>
2017-10-02 21:21:42 -0700

    Update items_helper.rb

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,23 +1,23 @@
 module ItemsHelper
 
 	def self.search(input)
-  	items = Item.all
-  	matched_items = []
+		items = Item.all
+		matched_items = []
 
-  	items.each do |item|
-  		
-  		if item.name.downcase.include?(input.downcase)
-      	matched_items << item
-      end
-      
-      if item.tags
-	      if item.tags.downcase.include?(input.downcase)
-	      	matched_items << item
-	      end  
-	    end
-  	end
+		items.each do |item|
 
-    return matched_items
-  end
+			if item.name.downcase.include?(input.downcase)
+				matched_items << item
+			 end
+
+			 if item.tags
+				 if item.tags.downcase.include?(input.downcase)
+					matched_items << item
+				end  
+			  end
+		end
+
+		return matched_items
+	end
 
 end
