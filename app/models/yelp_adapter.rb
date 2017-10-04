e990eecf54534734e0b6e37127851e2c4c5284ef
Danielle Glass <danielleglass@me.com>
2017-10-04 13:49:38 -0700

    correct user registration to pass access token

--- app/models/yelp_adapter.rb
+++ app/models/yelp_adapter.rb
@@ -1,70 +1,37 @@
 module YelpAdapter
   class << self
 
-require "json"
-require "http"
-require "optparse"
+  require "json"
+  require "http"
+  require "optparse"
+
+  # Constants, do not change these
+  API_HOST = "https://api.yelp.com"
+  SEARCH_PATH = "/v3/businesses/search"
+  BUSINESS_PATH = "/v3/businesses/"
+  # DEFAULT_LOCATION = "San Francisco, CA"
+  SEARCH_LIMIT = 5
+
+  def longitude(latitude)
+    longitude = longitude
+  end
 
 
-# Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
-# from https://www.yelp.com/developers/v3/manage_app
-CLIENT_ID = ENV["CLIENT_ID"]
-CLIENT_SECRET = ENV["CLIENT_SECRET"]
+  # Returns a parsed json object of the request
+    def search(term)
+      url = "#{API_HOST}#{SEARCH_PATH}"
+      params = {
+        term: term,
+        # location: DEFAULT_LOCATION,
+        latitude: latitude,
+        longitude: longitude,
+        limit: SEARCH_LIMIT,
+        open_now: true
+      }
 
+      response = HTTP.auth(ENV["AUTHORIZATION"]).get(url, params: params)
 
-# Constants, do not change these
-API_HOST = "https://api.yelp.com"
-SEARCH_PATH = "/v3/businesses/search"
-BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
-# TOKEN_PATH = "/oauth2/token"
-# GRANT_TYPE = "client_credentials"
-
-
-DEFAULT_BUSINESS_ID = "yelp-san-francisco"
-# DEFAULT_TERM = "grocery"
-DEFAULT_LOCATION = "San Francisco, CA"
-SEARCH_LIMIT = 5
-
-
-# Returns a parsed json object of the request
-def search(term)
-  url = "#{API_HOST}#{SEARCH_PATH}"
-  params = {
-    term: term,
-    # location: DEFAULT_LOCATION,
-    latitude: 37.7618770,
-    longitude: -122.4149990,
-    limit: SEARCH_LIMIT,
-    open_now: true
-  }
-
-  response = HTTP.auth(ENV["AUTHORIZATION"]).get(url, params: params)
-
-  response.parse
-end
-
-
-# Look up a business by a given business id. Full documentation is online at:
-# https://www.yelp.com/developers/documentation/v3/business
-#
-# business_id - a string business id
-#
-# Examples
-#
-#   business("yelp-san-francisco")
-#   # => {
-#          "name": "Yelp",
-#          "id": "yelp-san-francisco"
-#          ...
-#        }
-#
-# Returns a parsed json object of the request
-# def business(business_id)
-#   url = "#{API_HOST}#{BUSINESS_PATH}#{business_id}"
-
-#   response = HTTP.auth(ENV["AUTHORIZATION"]).get(url)
-#   response.parse
-# end
-
-end
+      response.parse
+    end
+  end
 end
